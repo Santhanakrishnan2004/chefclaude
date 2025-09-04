dbd9d5f628f6a8fa13c7e8d82a04d521d15c9bd8
Santhana Krishnan <118243369+Santhanakrishnan2004@users.noreply.github.com>
2025-09-04 10:58:59 +0530

    Update ai.js

--- src/ai.js
+++ src/ai.js
@@ -1,6 +1,7 @@
 const SYSTEM_PROMPT = `
 You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page.
 `
+console.log("Vercel API Key exists?", !!import.meta.env.VITE_GAPI);
 
 // Load API key from Vite env
 const API_KEY = import.meta.env.VITE_GAPI;
